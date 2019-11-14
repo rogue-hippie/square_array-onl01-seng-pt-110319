38e9a813a147af342c5fefa0683cc1d1db5ef756
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-14 20:55:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each do |numbers|
     new_array << numbers**2 
   end
   new_array
+=======
+  array.each do |number|
+    puts #{number}**2 
+  end
+>>>>>>> 614990ce4d3b8d727474ff1d0e4c519bc1bcf8b0
 end
